07532534cc33a749bbb99c85b5947d5e60fdef0c
Jonathan Compton <jonathankcompton@gmail.com>
2022-06-26 16:05:02 -0700

    fixed linting errors

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,10 +1,12 @@
-""" 
+"""
 Calculator Function
 """
+
 
 def add(x, y):
     """ Add x and y and return value """
     return x + y
+
 
 def subtract(y, x):
     """ Subtract y from x and return result """
--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -8,6 +8,7 @@ from psycopg2 import OperationalError as Psycopg2Error
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
     """ Django command to wait for database."""
 
@@ -23,5 +24,3 @@ class Command(BaseCommand):
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
         self.stdout.write(self.style.SUCCESS('Database available!'))
-
-
